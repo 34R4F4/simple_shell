04d2762a80b04238f710c21257565a08549943d0
Arafa <khalafarafa@gmail.com>
2023-12-13 14:49:39 -0800

    Shell-00

--- shell.h
+++ shell.h
@@ -9,41 +9,6 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-/*
- * Allowed functions and system calls:
- *
- * access
- * chdir
- * close
- * closedir
- * execve
- * exit
- * _exit
- * fflush
- * fork
- * free
- * getcwd
- * getline
- * getpid
- * isatty
- * kill
- * malloc
- * open
- * opendir
- * perror
- * read
- * readdir
- * signal
- * stat (__xstat)
- * lstat (__lxstat)
- * fstat (__fxstat)
- * strtok
- * wait
- * waitpid
- * wait3
- * wait4
- * write
- */
 
 
 
